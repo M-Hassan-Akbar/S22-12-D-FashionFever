7c2ca71e206ba7eeb3a8f289d67fb1fe8b1ffa2a
M. Hassan Akbar <hasxanblaze@gmail.com>
2022-10-25 21:26:02 +0500

    testing

--- backend/test.py
+++ backend/test.py
@@ -1,4 +1,21 @@
-from attngan.evaluation import gen_image
+import pyrebase
+
+config = {
+    'apiKey': "AIzaSyAEIeJDRhU_cOg2YC3UESpXZFxpJDHOkTs",
+    'authDomain': "fashionfever-2.firebaseapp.com",
+    'projectId': "fashionfever-2",
+    'storageBucket': "fashionfever-2.appspot.com",
+    'messagingSenderId': "116748259026",
+    'appId': "1:116748259026:web:4a250e2aa206a78936e70b",
+    'measurementId': "G-ZDV2X5HE6R",
+    'databaseURL' : 'https://fashionfever-2-default-rtdb.asia-southeast1.firebasedatabase.app/'
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+storage = firebase.storage()
 
 
-gen_image("A bird flying in ocean")
+
+
+storage.put("https://media.discordapp.net/attachments/737338724999888918/1034450009288093716/unknown.png?width=1193&height=671 ")
