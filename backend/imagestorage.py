54bf08be7a5648f2436208fd6b327828f0e2476c
M. Hassan Akbar <hasxanblaze@gmail.com>
2022-12-28 21:24:57 +0500

    Added Measurements and User search functionality

--- backend/imagestorage.py
+++ backend/imagestorage.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 CORS(app)
 
 config = {
-    'apiKey': "AIzaSyAEIeJDRhU_cOg2YC3UESpXZFxpJDHOkTs",
-    'authDomain': "fashionfever-2.firebaseapp.com",
-    'projectId': "fashionfever-2",
-    'storageBucket': "fashionfever-2.appspot.com",
-    'messagingSenderId': "116748259026",
-    'appId': "1:116748259026:web:4a250e2aa206a78936e70b",
-    'measurementId': "G-ZDV2X5HE6R",
-    'databaseURL' : 'https://fashionfever-2-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    "apiKey": "AIzaSyAEIeJDRhU_cOg2YC3UESpXZFxpJDHOkTs",
+    "authDomain": "fashionfever-2.firebaseapp.com",
+    "projectId": "fashionfever-2",
+    "storageBucket": "fashionfever-2.appspot.com",
+    "messagingSenderId": "116748259026",
+    "appId": "1:116748259026:web:4a250e2aa206a78936e70b",
+    "measurementId": "G-ZDV2X5HE6R",
+    "databaseURL": "https://fashionfever-2-default-rtdb.asia-southeast1.firebasedatabase.app/",
 }
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -31,47 +31,52 @@ def index():
 @app.route("/getimages", methods=["GET", "POST"])
 def fetchImage():
     email = request.json["email"]
-    
+
     images = db.child("images").get()
 
     image_list = []
     for image in images.each():
-        if(image.val()["email"] == email):
+        if image.val()["email"] == email:
             image_list.append(db.child("images").child(image.key()).get().val())
-    
-    return jsonify({"images" : image_list})
+
+    return jsonify({"images": image_list})
 
 
 @app.route("/addad", methods=["POST"])
 def addAd():
-    email = request.args.get('email')
-    description = request.args.get('description')
-    phone_number = request.args.get('phone_number')
-    full_name = request.args.get('full_name')
-    url = request.args.get('url')
-    
+    email = request.args.get("email")
+    description = request.args.get("description")
+    phone_number = request.args.get("phone_number")
+    full_name = request.args.get("full_name")
+    url = request.args.get("url")
+
     if len(request.files):
-        file = request.files['file']
-        name = "images/"+ email + str(random.randrange(100000000000000, 1000000000000000)) + ".jpg"
+        file = request.files["file"]
+        name = (
+            "images/"
+            + email
+            + str(random.randrange(100000000000000, 1000000000000000))
+            + ".jpg"
+        )
         storage.child(name).put(file)
 
         url = storage.child(name).get_url(None)
-        db.child("images").push({
-                'email' : email,
-                'name' : name,
-                'caption' : '',
-                'url' : url
-            })
+        db.child("images").push(
+            {"email": email, "name": name, "caption": "", "url": url}
+        )
 
-    db.child("ads").push({
-        'email' : email,
-        'name' : full_name,
-        'description' : description,
-        'phone_number' : phone_number,
-        'url' : url
-    })
+    db.child("ads").push(
+        {
+            "email": email,
+            "name": full_name,
+            "description": description,
+            "phone_number": phone_number,
+            "url": url,
+        }
+    )
 
     return jsonify({"ad": "created"})
+
 
 @app.route("/getad", methods=["POST", "GET"])
 def getAd():
@@ -80,13 +85,9 @@ def getAd():
     getads = []
     for ad in ads.each():
         getads.append(ad.val())
-    
-    return jsonify({"ads" : getads})
-    
-    
+
+    return jsonify({"ads": getads})
+
 
 if __name__ == "__main__":
-    app.run(port=5002) 
-    
-
-
+    app.run(port=5002)
